eb6753b6da0013880c27d93007455b83b17e11b9
Tumaini Wekesa <tumainiwamukota@gmail.com>
2025-06-20 13:29:05 +0300

    Selection Sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,24 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    
+    # print(arr)
+    
+    for i in range (n):
+        min_index = i
+        
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+                
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        
+    print("Sorted List: ", arr)
+    
+    
+    
+    
+def main():
+    arr = list(map(int, input("Enter elements, space: ").split()))
+    selection_sort(arr)
+    
+main()
